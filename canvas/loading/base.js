4984704a23d091cf566ba7a09db6273aa805e889
leoMelody <760282292@qq.com>
2018-08-06 09:26:23 +0800

    loading canvas 学习

---
+++ canvas/loading/base.js
@@ -0,0 +1,11 @@
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext('2d')
+
+// 全屏canvas
+canvas.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+canvas.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+function init() {
+  ctx.beginPath()
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+}
