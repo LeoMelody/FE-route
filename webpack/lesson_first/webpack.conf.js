36afd3cb747e8ad927acc604ad35135906b7bee1
王一衡 <760282292@qq.com>
2018-08-24 11:13:18 +0800

    babel

--- webpack/lesson_first/webpack.conf.js
+++ webpack/lesson_first/webpack.conf.js
@@ -4,5 +4,14 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: '/\.js$/', // 提供正则来匹配.js结尾的文件
+        // use: ['babel-loader'] // 匹配到这一类文件后，将其交给哪些loader去处理，这里我们先配置为最简单的模式
+        loader: 'babel-loader'
+      }
+    ]
   }
 }
