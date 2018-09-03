ce3213181c327422635c6cf895e4a14eccb83d05
王一衡 <760282292@qq.com>
2018-09-03 17:12:40 +0800

    lesson2

--- webpack/lesson_2ed/src/App.vue
+++ webpack/lesson_2ed/src/App.vue
@@ -1,16 +1,5 @@
 <template>
-  <div class="body">
-    我是第一版测试代码，devServer完成
-    <!-- <div class="icon">
-      <img src="./assets/star.png" alt="" srcset="" width="200" height="200">
-      <img src="./assets/cz.png" alt="" width="100" height="100">
-    </div> -->
-
-    <div class="icon-big">
-    </div>
-
-    <div class="icon-sm"></div>
-  </div>
+  <router-view></router-view>
 </template>
 
 <script>
@@ -29,27 +18,4 @@ export default {
   }
 }
 </script>
-
-<style lang="scss" scoped>
-$height: 200px;
-$fontColor: rgb(66, 252, 29);
-@mixin bg($url, $w, $h) {
-  background: url($url) no-repeat  0px 0px;
-  background-size: $w $h;
-}
-
-.body {
-  color: $fontColor;
-  .icon-big {
-    height: $height;
-    @include bg('./assets/star.png', 200px, 200px);
-  }
-
-  .icon-sm {
-    height: $height;
-    @include bg('./assets/cz.png', 50px, 50px);
-  }
-}
-
-</style>
 
--- webpack/lesson_2ed/src/index.js
+++ webpack/lesson_2ed/src/index.js
@@ -1,8 +1,12 @@
 import Vue from 'vue/dist/vue.esm'
 import App from './App.vue'
+import router from './router'
+import store from './store'
 
 const vm = new Vue({
   el: '#app',
+  router,
+  store,
   components: { App },
   template: '<App/>'
 })
