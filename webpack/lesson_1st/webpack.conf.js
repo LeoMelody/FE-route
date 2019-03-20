a7fb3f4e212ae6da90cb05b13a7a6ac395accd4b
hellowyh <wangyiheng@ddjf.com.cn>
2019-03-20 19:07:47 +0800

    配置修改

--- webpack/lesson_1st/webpack.conf.js
+++ webpack/lesson_1st/webpack.conf.js
@@ -14,7 +14,7 @@ module.exports = {
         test: /\.js$/, // 提供正则来匹配.js结尾的文件
         // use: ['babel-loader'] // 匹配到这一类文件后，将其交给哪些loader去处理，这里我们先配置为最简单的模式
         loader: 'babel-loader',
-        exclude: '/node_modules/'
+        exclude: '/node_modules/' // babel-loader不去处理
       },
       {
         test: /\.vue$/, // 匹配.vue结尾的文件
