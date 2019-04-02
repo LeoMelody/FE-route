a6b2011dce399e8eb60f6f9aec0f5c71acc21010
hellowyh <wangyiheng@ddjf.com.cn>
2019-04-02 14:02:56 +0800

    http预请求

--- http/server/index.js
+++ http/server/index.js
@@ -11,7 +11,10 @@ http.createServer((request,response) => {
   // 更好地设置权限,设置
   response.writeHead(200, {
     // 这能设置一个值
-    "Access-Control-Allow-Origin": "http://localhost:3001"
+    "Access-Control-Allow-Origin": "http://localhost:3001",
+    "Access-Control-Allow-Headers": "X-Test-Cors",
+    "Access-Control-Allow-Methods": "PUT, DELETE",
+    "Access-Control-Max-Age": '1000' // 设置1000s内不再做请求限制
   })
   response.end('123') // 成功接收到请求
 }).listen(3000)
