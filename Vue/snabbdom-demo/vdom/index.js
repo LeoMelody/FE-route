4a3494aac2e00d28ee97bc4747f921ae20beee4a
hellowyh <wangyiheng@ddjf.com.cn>
2019-08-06 09:20:37 +0800

    Diff简单了解

--- Vue/snabbdom-demo/vdom/index.js
+++ Vue/snabbdom-demo/vdom/index.js
@@ -1,3 +1,23 @@
+/*
+ * @Author: leo 
+ * @Date: 2019-08-05 00:48:43 
+ * @Last Modified by: leo
+ * @Last Modified time: 2019-08-05 00:58:42
+ */
+// init snabbdom
+const snabbdom = window.snabbdom
+// patch 函数
+const patch = snabbdom.init({
+  snabbdom_class,
+  snabbdom_props,
+  snabbdom_style,
+  snabbdom_eventlisteners
+})
+
+const h = snabbdom.h
+
+// init data
+
 let dataList = [
   {
     name: 'wyh',
@@ -19,3 +39,23 @@ btn.addEventListener('click', (e) => {
   let val = input.value
   update(val)
 })
+
+// create vNode
+function create(data = []) {
+  return h('div#app', {}, data.map(item => {
+    return [
+      h('div', {}, item.name),
+      h('div', {}, item.age)
+    ]
+  }).flat(Infinity))
+}
+
+function update(val) {
+  dataList[0].name = val
+  var newVnode = create(dataList)
+  patch(vnode, newVnode)
+}
+
+var vnode = create(dataList)
+
+patch(wrap, vnode)
