d5267c2e2d4e51f50256c484d1d918083a7727fb
leohengwang <leohengwang@tencent.com>
2019-08-21 22:55:17 +0800

    提交测试

---
+++ H5/crit-game/index.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: mikey.zhaopeng 
+ * @Date: 2019-08-21 21:44:58 
+ * @Last Modified by: leo
+ * @Last Modified time: 2019-08-21 22:54:08
+ * 暴击游戏测试
+ */
+
+function main() {
+	
+}
+
+main()
