187ac16ad301ab945e17e8e1f3947e7f957be87d
leohengwang <leohengwang@tencent.com>
2019-08-25 14:43:09 +0800

    增加game.js

--- H5/crit-game/index.js
+++ H5/crit-game/index.js
@@ -1,13 +1,23 @@
 /*
- * @Author: mikey.zhaopeng 
- * @Date: 2019-08-21 21:44:58 
+ * @Author: leo 
+ * @Date: 2019-08-21 22:58:23 
  * @Last Modified by: leo
- * @Last Modified time: 2019-08-21 22:54:08
- * 暴击游戏测试
+ * @Last Modified time: 2019-08-22 12:13:43
  */
 
-function main() {
-	
-}
+function main() {}
 
 main()
+
+class Player {
+  constructor(info = {
+    // 玩家基础信息
+  }) {
+    
+  }
+
+  static createPlayer() {
+    // 暂时不需要设置单例
+    return new Player()
+  }
+}
