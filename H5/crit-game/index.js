abfb63d2a3f2afd6ebb0a5bc254d8a09409365f5
leohengwang <leohengwang@tencent.com>
2019-08-27 22:08:26 +0800

    base player

--- H5/crit-game/index.js
+++ H5/crit-game/index.js
@@ -2,7 +2,7 @@
  * @Author: leo 
  * @Date: 2019-08-21 22:58:23 
  * @Last Modified by: leo
- * @Last Modified time: 2019-08-22 12:13:43
+ * @Last Modified time: 2019-08-27 22:08:18
  */
 
 function main() {}
@@ -13,7 +13,20 @@ class Player {
   constructor(info = {
     // 玩家基础信息
   }) {
-    
+    this.atack = info.atack || 100
+    // 假定天选之子，暴击最多为0.01 ~ 0.09
+    this.crit = info.crit || +(Math.random()*0.1).toFixed(2)
+    // 初始化暴击伤害为 150%
+    this.critDamage = info.critDamage || 1.5
+    // 攻击频率 一秒一次
+    this.atackFrequency = info.atackFrequency || 1
+  }
+
+  /**
+   * 攻击时间
+   */
+  continueAtach(time = 0) {
+
   }
 
   static createPlayer() {
